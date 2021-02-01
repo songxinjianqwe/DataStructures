53fe5e9ac1605990e3a20bfd4b5c003e9ed60010
xinjian <xinjian@xinjiandeMacBook-Pro.local>
2021-02-02 00:17:58 +0800

    ok

--- src/main/java/com/sinjinsong/leetcode/medium/LeetCode31.java
+++ src/main/java/com/sinjinsong/leetcode/medium/LeetCode31.java
@@ -1,7 +1,20 @@
 package com.sinjinsong.leetcode.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode31 {
     public void nextPermutation(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int index = nums.length - 1;
+        List<Integer> ans = new ArrayList<>();
+        while(index >= 0) {
+            if(nums[index] < max) {
 
+            }
+        }
     }
 }
